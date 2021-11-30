f9d58d6a6468536a44f3e40d645f54461a5ff43f
vkuntz23 <89659604+vkuntz23@users.noreply.github.com>
2021-11-30 00:49:45 -0500

    commit

--- app.R
+++ app.R
@@ -1,3 +1,4 @@
+
 #
 # This is a Shiny web application. You can run the application by clicking
 # the 'Run App' button above.
@@ -8,42 +9,21 @@
 #
 
 library(shiny)
+library(readr)
+library(leaflet)
+library(dplyr)
+library(rgdal)
+library(RColorBrewer)
 
-suicideData <- read_csv('fullSuicideData.csv')
+suicideData <- read_csv('suicideRatesWideYears.csv')
 leafletData <- readOGR(
     dsn = paste0(getwd(), "/data/LeafletData"),
     layer = "TM_WORLD_BORDERS_SIMPL-0.3",
     verbose = FALSE
 )
-suicideData2016 <- suicideData %>%
-    filter(Year == "2016")
-leafletSuicideData <- merge(leafletData, suicideData2016, by = "ISO3") 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("year",
-                        "Year:",
-                        min = 1990,
-                        max = 2017,
-                        value = 2017,
-                        sep = "")
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           leafletOutput("map")
-        )
-    )
-)
-
-mybins <- c(0,1,2,5,7,10,12, 15, 20, 25, 30, Inf)
-mypalette <- colorBin( 
+leafletSuicideData <- merge(leafletData, suicideData, by = "ISO3") 
+mybins <- c(0,1,2,5,7,10,12, 15, 20, 25, 30, 50, 100)
+myPalette <- colorBin( 
     palette=c("#333399",
               "#555599", 
               "#777799",
@@ -54,48 +34,128 @@ mypalette <- colorBin(
               "#ff7777", 
               "#cc5555",
               "#aa0000",
-              "#550000"),
-    domain = leafletSuicideData2016@data$SuicideDeathRate,
+              "#550000",
+              "#330000"),
+    domain = leafletSuicideData$Rate2017,
     na.color = "transparent",
     bins = mybins
 )
 
+# Define UI for application that draws a histogram
+ui <- fluidPage(
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
+    # Application title
+    titlePanel("World Suicide Rates"),
 
-    leafletSuicideData <- reactive(
-        merge(leafletData, filter(suicideData, Year == input$year), by = "ISO3")
+    # Sidebar with a slider input for the year
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("year",
+                        "Year:",
+                        min = 1990,
+                        max = 2017,
+                        value = 2017,
+                        sep = "",
+                        animate = animationOptions(interval = 750, loop = TRUE))
+        ),
+
+        # Show a map of suicide rates
+        mainPanel(
+           leafletOutput("map", height = 600)
+        )
     )
+)
+
+
+
+
+
+# Define server logic required to draw maps of suicide rates
+server <- function(input, output) {
     output$map <- renderLeaflet({
-        # Prepare the text for tooltips:
-        hoverText <- paste(
-            "Country: ", leafletSuicideData()@data$NAME, "<br/>",
-            "Total MH Beds: ", leafletSuicideData()@data$TOTAL_BEDS, "<br/>",
-            "Suicide Rate: ", round(leafletSuicideData()@data$SuicideDeathRate, 2), "<br/>",
-            "Ratio of male to female suicide: ", round(leafletSuicideData()@data$MaleFemaleSuicideRatio, 2),
-            sep = ""
-        ) %>%
-            lapply(htmltools::HTML)
-        
-        # Final Map
-        leaflet(leafletSuicideData()) %>%
+        # Draw the base map and legend
+        leaflet(leafletSuicideData) %>%
             addTiles() %>%
-            setView(lat = 10, lng = 0, zoom = 2) %>%
-            addPolygons(
-                fillColor = ~ mypalette(SuicideDeathRate),
-                stroke = TRUE,
-                fillOpacity = 1,
-                color = "white",
+            setView(lat = 30, lng = 0, zoom = 1.5) %>%
+            addLegend(pal = myPalette, values = leafletSuicideData$Rate2017, opacity = .7, title = "Suicide Death Rate<br/>Per 100k", position = "bottomleft")
+    })
+    
+    
+    observe({
+        # Select the right column based on the selected year        
+        selectedData = switch(as.character(input$year),
+         "1990" = leafletSuicideData$Rate1990,
+         "1991" = leafletSuicideData$Rate1991,
+         "1992" = leafletSuicideData$Rate1992,
+         "1993" = leafletSuicideData$Rate1993,
+         "1994" = leafletSuicideData$Rate1994,
+         "1995" = leafletSuicideData$Rate1995,
+         "1996" = leafletSuicideData$Rate1996,
+         "1997" = leafletSuicideData$Rate1997,
+         "1998" = leafletSuicideData$Rate1998,
+         "1999" = leafletSuicideData$Rate1999,
+         "2000" = leafletSuicideData$Rate2000,
+         "2001" = leafletSuicideData$Rate2001,
+         "2002" = leafletSuicideData$Rate2002,
+         "2003" = leafletSuicideData$Rate2003,
+         "2004" = leafletSuicideData$Rate2004,
+         "2005" = leafletSuicideData$Rate2005,
+         "2006" = leafletSuicideData$Rate2006,
+         "2007" = leafletSuicideData$Rate2007,
+         "2008" = leafletSuicideData$Rate2008,
+         "2009" = leafletSuicideData$Rate2009,
+         "2010" = leafletSuicideData$Rate2010,
+         "2011" = leafletSuicideData$Rate2011,
+         "2012" = leafletSuicideData$Rate2012,
+         "2013" = leafletSuicideData$Rate2013,
+         "2014" = leafletSuicideData$Rate2014,
+         "2015" = leafletSuicideData$Rate2015,
+         "2016" = leafletSuicideData$Rate2016,
+         "2017" = leafletSuicideData$Rate2017
+         )
+
+        hoverText <- 
+            paste(
+              "Country: ", leafletSuicideData@data$NAME, "<br/>",
+               "Suicide Rate (", 
+                     input$year, 
+                     "): ", selectedData, "<br/>",
+                     sep = "") %>%
+                     lapply(htmltools::HTML)
+            
+        leafletProxy('map', data = leafletSuicideData) %>%
+            clearGroup("circles") %>%
+            addCircles(
+                lat = leafletSuicideData$LAT, lng = leafletSuicideData$LON,
+                radius = selectedData * 10000,
+                opacity = 1,
+                fillOpacity = .7,
                 weight = 1,
+                group = "circles",
+                color = myPalette(selectedData),
+                fillColor = myPalette(selectedData),
                 label = hoverText,
                 labelOptions = labelOptions(
                     style = list("font-weight" = "normal", padding = "3px 8px"),
                     textsize = "13px",
                     direction = "auto"
                 )
-            ) %>%
-            addLegend(pal = mypalette, values = ~SuicideDeathRate, opacity = 1, title = "Suicide Death Rate", position = "bottomleft")
+            ) 
+            # addPolygons(
+            #     fillColor = ~ myPalette(selectedData),
+            #     stroke = TRUE,
+            #     fillOpacity = 1,
+            #     color = "white",
+            #     weight = 1,
+            #     label = hoverText(),
+            #     labelOptions = labelOptions(
+            #         style = list("font-weight" = "normal", padding = "3px 8px"),
+            #         textsize = "13px",
+            #         direction = "auto"
+            #     )
+            # ) 
+
+            
     })
 }
 
